f682a33cfbf2425a0199663294e122f8eb53a89a
Agustin Peralta <aeperalta@uc.cl>
2021-06-30 09:39:02 -0400

    attributes item loaded in order for shadow, delete console logs, little style changes in todo lit

--- src/views/todo-shadow.js
+++ src/views/todo-shadow.js
@@ -5,7 +5,9 @@ template.innerHTML = /*html*/`
         .container {
             display: flex;
             flex-direction: column;
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
             width: 500px;
+            padding: 10px;
         }
         .add-button {
             margin-left: 10px
@@ -41,8 +43,36 @@ class ToDoList extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('title');
         this.shadowRoot.querySelector('h4').innerText = 'Para que te organices, ' + this.getAttribute('name');
+        this.shadowRoot.querySelector('input').placeholder = this.getAttribute('placeholder')
     }
+
+    inWords (num) {
+        let a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+        let b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+        if ((num = num.toString()).length > 9) return 'overflow';
+        let n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+        if (!n) return; let str = '';
+        str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+        str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+        str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+        str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+        str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) : '';
+        return str.replace(/\s+/g, '');
+    }
+    
+
+    getInicialToDos() {
+        for (let index = 1; index < 101; index++) {
+          let name = 'item' + this.inWords(index);
+          const val = this.getAttribute(name);
+          if (val){
+            this.addToDo(val)
+          }
+        }
+    }  
+
     addToDo(val) {
         var div = document.createElement( 'todo-item-shadow' );
         div.setAttribute('content',  val)
@@ -56,14 +86,8 @@ class ToDoList extends HTMLElement {
     }
     connectedCallback(){
         const button = this.shadowRoot.querySelector('button');
-        button.addEventListener('click', this.addToDoBind.bind( this ))
-        const attributeNames = this.getAttributeNames();
-        attributeNames.forEach(attr => {
-            if (attr.includes('item')) {
-                const val = this.getAttribute(attr);
-                this.addToDo(val);
-            }
-        })
+        button.addEventListener('click', this.addToDoBind.bind( this ));
+        this.getInicialToDos()
     }
     disconnectedCallback(){
 
