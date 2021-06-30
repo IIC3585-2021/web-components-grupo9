a06c44c178d9bf754a469c735ff97a78f3e62c97
Pablo Garnham <pgarnham@uc.cl>
2021-06-30 00:45:02 -0400

    working with 100 items

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -33,17 +33,37 @@ class TodoView extends LitElement {
         this.title = 'My Tasks'
     }
 
+    
+
+  inWords (num) {
+    let a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    let b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    if ((num = num.toString()).length > 9) return 'overflow';
+    let n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; let str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) : '';
+    return str.replace(/\s+/g, '');
+  }
+
+
     getInicialToDos() {
       let initialTodos = [];
-      let names = ["first", "second", "third"]
-      names.forEach(name => {
-        if (this.getAttribute(name + "item")){
+      console.log("partiendo")
+      for (let index = 1; index < 101; index++) {
+        let name = 'item' + this.inWords(index);
+        console.log(name);
+        if (this.getAttribute(name)){
+          console.log("entrando")
           initialTodos.push({ 
-            task: this.getAttribute(name + "item"),
+            task: this.getAttribute(name),
             complete: false
         })
         }
-      })
+      }
       return initialTodos
     }
 
@@ -130,7 +150,6 @@ class TodoView extends LitElement {
       )
     }
       </div>
-      <br><br>
       <div class="input-layout" @keyup="${this.shortcutListener}">
         <input placeholder="${this.placeholder}" value="${this.task}" @change="${this.updateTask}">
         <button @click="${this.addTodo}" class="todo-element" id="search-button">
